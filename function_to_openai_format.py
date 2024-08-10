3b024aff1cd633fbbb2ed93fd3cf774dd065164e
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-08-10 16:59:04 +0530

    Update function_to_openai_format.py
    Update with functions to parse and remove #def paths in the json function (when objects of one class are passed as the attributes of another class created from the pydantic BaseModel

--- function_to_openai_format.py
+++ function_to_openai_format.py
@@ -35,3 +35,60 @@ def remove_titles(kv: dict, prev_key: str = "", ) -> dict: #_infer_skip_keys fro
 			else:
 				new_kv[k] = v
 		return new_kv
+
+def dereference_refs_helper(obj, full_schema, skip_keys, processed_refs=None):
+		'''
+		Will remove from the obj schema the entire key, value pair where the key==skip_key
+		'''
+		full_schema = obj if not full_schema else full_schema
+		if processed_refs is None:
+			processed_refs = set()
+
+		if isinstance(obj, dict):
+			obj_out = {}
+			for k, v in obj.items():
+				if k in skip_keys:
+					obj_out[k] = v
+				elif k == "$ref":
+					if v in processed_refs:
+						continue
+					processed_refs.add(v)
+					ref = retrieve_ref(v, full_schema)
+					full_ref = dereference_refs_helper(
+						ref, full_schema, skip_keys, processed_refs
+					)
+					processed_refs.remove(v)
+					return full_ref
+				elif isinstance(v, (list, dict)):
+					obj_out[k] = dereference_refs_helper(
+						v, full_schema, skip_keys, processed_refs
+					)
+				else:
+					obj_out[k] = v
+			return obj_out
+		elif isinstance(obj, list):
+			return [
+				dereference_refs_helper(el, full_schema, skip_keys, processed_refs)
+				for el in obj
+			]
+		else:
+			return obj
+		
+def retrieve_ref(path, schema): #_retrieve_ref in langchain
+		print('Original path in retrieve_ref:\t', path)
+		components = path.split("/")  # path = '#/$defs/Bar' ; schema = {'$ref': '#/$defs/Bar'} in example
+		if components[0] != "#":
+			raise ValueError(
+			"ref paths are expected to be URI fragments, meaning they should start with #."
+		)
+		out = schema
+		print('Original schema in retrieve_ref:\t',out)
+		for component in components[1:]: # [$defs,Bar]
+			print('Component in retrieve_ref:\t', component)
+			if component in out:
+				out = out[component]
+			elif component.isdigit() and int(component) in out:
+				out = out[int(component)]
+			else:
+				raise KeyError(f"Reference '{path}' not found.")
+		return deepcopy(out)
